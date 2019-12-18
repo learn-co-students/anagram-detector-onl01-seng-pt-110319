e5056f9b5fc3aa29be7a6f7948d8a20dfd6a2388
arnie b <arnold.bwaila@gmail.com>
2019-12-18 16:58:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  @word
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(words)
+  sorted_word=@word.split.sort
+  sorted_words=[]
+  
+  words.each { |wrd| sorted_words  = wrd.split.sort
