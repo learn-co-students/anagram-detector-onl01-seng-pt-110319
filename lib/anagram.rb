3de0c18993c6373375122223c3f610e88f97cbb7
arnie b <arnold.bwaila@gmail.com>
2019-12-18 19:45:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,26 @@ def initialize(word)
 end
 
 def match(words)
-  sorted_word=@word.split.sort
-  sorted_words=[]
+  sorted_word = @word.split.sort
+  sorted_words = []
+  matches = []
+  words.each do |wrd| 
+    sorted_words  = wrd.split.sort 
+    matched? = true
+    if sorted_words.length == sorted_word.length
+      i = 0
+      sorted_words.each do |chr|
+        if sorted_word[i] != chr {matched? = false}
+         i++
+      end # end of sorted_words.each
+    else matched? = false
+    end #end of if sorted_words
+    if matched? == true { matches << wrd }
+  end #end of words.each
+  matches
+end #end of method match
+
+    
   
-  words.each { |wrd| sorted_words  = wrd.split.sort
+       
+       
