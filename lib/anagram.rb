5bc2098f3a3752f12c3cd5ac21204f9d06341662
arnie b <arnold.bwaila@gmail.com>
2019-12-18 19:57:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,11 +13,11 @@ def match(words)
   matches = []
   words.each do |wrd| 
     sorted_words  = wrd.split.sort 
-    matched? = true
+    matched = true
     if sorted_words.length == sorted_word.length
       i = 0
       sorted_words.each do |chr|
-        if sorted_word[i] != chr {matched? = false}
+        if sorted_word[i] != chr {matched = false}
          i++
       end # end of sorted_words.each
     else matched? = false
